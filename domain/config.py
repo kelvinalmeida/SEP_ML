ead6fb0d53e8b28bbd0defb78dc2afc7046d90c9
Kelvin Santos <kelvinsantosdealmeida@gmail.com>
2025-11-27 15:36:46 -0300

    teste 2

---
+++ domain/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv('config.env')
+
+
+class Config:
+    # SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # PUSHALERT_API_KEY = os.getenv('PUSHALERT_API_KEY')
